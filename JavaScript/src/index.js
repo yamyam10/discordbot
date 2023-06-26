a1b6b8378e8b54d7f964797c444a7994dbac81a5
{Niiii} <tapioka2real@gmail.com>
2023-06-26 11:44:47 +0900

    javascriptでもbotを使えるようにするため基礎を構築

---
+++ JavaScript/src/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const {token} = process.env;
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
+});
+
+//起動確認
+client.once('ready', () => {
+    console.log(`${client.user.tag} Ready`);
+});
+
+//返答
+client.on('messageCreate', message => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content == 'hi') {
+        message.channel.send('hi!');
+    }
+});
+
+//Discordへの接続
+client.login(token);
